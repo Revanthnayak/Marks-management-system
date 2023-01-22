4a395331682f81fd71cb6641fede5e198b260442
Revanthnayak <revanthtn_cs20.rvitm@rvei.edu.in>
2023-01-22 19:52:59 +0530

    rankerpage

--- html/ranker.php
+++ html/ranker.php
@@ -52,31 +52,12 @@
     </table>
     <?php 
 $connect=mysqli_connect("localhost","root","","user_db") or die("connection failed");
-$query="SELECT stuid,me,me1,me2,cn,cn1,cn2,dbms,dbms1,dbms2,atc,atc1,atc2,adp,adp1,adp2,unix,unix1,unix2,avg((me+me1+me2)/3) as totalme,avg((cn+cn1+cn2)/3) as totalcn,avg((dbms+dbms1+dbms2)/3) as totaldbms,avg((atc+atc1+atc2)/3) as totalatc,avg((adp+adp1+adp2)/3) as totaladp,avg((unix+unix1+unix2)/3) as totalunix from user_data,user_data1,user_data2 WHERE stuid=stuid1 AND stuid1=stuid2 group by (stuid) ";
+$query="select * from combined_table group by(stuid) order by (stuid)";
 $result=mysqli_query($connect,$query);
 while($row=mysqli_fetch_assoc($result))
 {
     ?>
     <tr>
-    <d><?php echo $row['stuid']?></d>
-        <d><?php echo $row['me']?></d>
-        <d><?php echo $row['cn']?></d>
-        <d><?php echo $row['dbms']?></d>
-        <d><?php echo $row['atc']?></d>
-        <d><?php echo $row['adp']?></d>
-        <d><?php echo $row['unix']?></d>
-        <d><?php echo $row['me1']?></d>
-        <d><?php echo $row['cn1']?></d>
-        <d><?php echo $row['dbms1']?></d>
-        <d><?php echo $row['atc1']?></d>
-        <d><?php echo $row['adp1']?></d>
-        <d><?php echo $row['unix1']?></d>
-        <d><?php echo $row['me2']?></d>
-        <d><?php echo $row['cn2']?></d>
-        <d><?php echo $row['dbms2']?></d>
-        <d><?php echo $row['atc2']?></d>
-        <d><?php echo $row['adp2']?></d>
-        <d><?php echo $row['unix2']?></d>
         <d><?php echo $row['totalme']?></d>
         <d><?php echo $row['totalcn']?></d>
         <d><?php echo $row['totaldbms']?></d>
